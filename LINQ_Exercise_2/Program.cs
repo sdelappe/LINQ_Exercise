0be85b3fb1eb9654a723c7ae9a691e379d0533e9
sdelappe <sdelappe57@gmail.com>
2021-10-14 20:03:18 -0700

    finished exercise 2

---
+++ LINQ_Exercise_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gameList = new List<string>
+            {
+                "Halo",
+                "Bioshock",
+                "Mario",
+                "Dead Space",
+                "Apex Legends",
+                "Pokemon",
+                "Metal Gear Solid"
+            };
+
+            var sortedList = gameList.OrderBy(str => str.Length);
+
+            foreach (var game in sortedList)
+            {
+                Console.WriteLine(game);
+            }
+
+            Console.WriteLine();
+
+            //for (int i = 0; i < sortedList.Count(); i++)
+            //{
+            //    Console.WriteLine();       * How do I access the strings within sortedList? *
+            //}
+        }
+    }
+}
